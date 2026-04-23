0a30db881c2b7616cd28bcdd9b55c74f4be878ee
Makan Garcia <makan.garcia@students.fhu.edu>
2026-04-22 20:18:24 -0500

    commiting to share the game window code. Next step is to make characters and a setting

--- game.py
+++ game.py
@@ -1,1 +1,22 @@
 import pygame
+from sys import exit
+
+#game variable
+GAME_WIDTH = 512
+GAME_HEIGHT = 512
+
+#Game Window
+pygame.init()
+window = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+pygame.display.set_caption("Game of WIP") #title in window
+clock = pygame.time.Clock() #frame rate
+
+while True: #game loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        pygame.display.update()
+        clock.tick(60) #60 FPS
+
