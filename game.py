3e9ab39400fb68785b813c87393e48a58c96993e
Noah Wilson <noah.wilson@students.fhu.edu>
2026-04-22 20:17:58 -0500

    finished the background

--- game.py
+++ game.py
@@ -1,1 +1,16 @@
+from tkinter import Image
+
 import pygame
+pygame.init()
+size = [1500, 800]
+window = pygame.display.set_mode(size)
+Image = pygame.image.load("download35.png")
+Image = pygame.transform.scale(Image, (1500, 800))
+done = False
+while not done:
+    pygame.display.flip()
+    window.blit(Image, (0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+pygame.quit()
