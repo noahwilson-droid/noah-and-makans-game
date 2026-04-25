12756200a6c5fe889b7b1d2b14e370606bb697c1
Makan Garcia <makan.garcia@students.fhu.edu>
2026-04-25 14:35:31 -0500

    I forgot to commit eariler. I deleted all the junk in the workspace. The update I made on the game is that there are now working health bars. I'm currently working on loading spritesheets and animations for the characters.

--- brawlers.py
+++ brawlers.py
@@ -2,11 +2,13 @@ import pygame
 
 class Brawlers():
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
         self.attacking = False
         self.attack_type = 0
+        self.health = 100
 
     def move(self, canvas_width, canvas_height, surface, target):
         SPEED = 10
@@ -52,15 +54,21 @@ class Brawlers():
             self.jump = False
             dy = canvas_height - 110 - self.rect.bottom
 
+        #Makes characters always face each other
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
         #updates player position
         self.rect.x += dx
         self.rect.y += dy       
 
     def attack(self, surface, target):
         self.attacking = True
-        attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
         if attacking_rect.colliderect(target.rect):
-            print("hit")
+            target.health -= 10
 
         pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
--- main.py
+++ main.py
@@ -14,17 +14,37 @@ pygame.display.set_caption("Battle of Lungunica")
 clock = pygame.time.Clock()
 FPS = 60
 
+#Color for Health Bar
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
 #Background/Setting
 bg_image = pygame.image.load("download35.png").convert_alpha()
+
+#spritesheets
+knight_sheet = pygame.image.load("Assets\Brawlers\knight.png").convert_alpha()
+samurai_sheet = pygame.image.load("Assets\Brawlers\Samurai.png").convert_alpha()
+
+#Steps of animation in each animation
+KNIGHT_ANIMATION_STEPS = [4, 4, 6, 2, 4, 6, 7]
+SAMURAI_ANIMATION_STEPS = [4, 5, 6, 3, 6, 9, 8]
 
 #function for drawing background
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (CANVAS_WIDTH, CANVAS_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
 
+#creating health bars. also I want to add assest to the bars to make them look cool
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
 #create two instances of fighters
-brawler_1 = Brawlers(200, 310)
-brawler_2 = Brawlers(700, 310)
+brawler_1 = Brawlers(200, 310, knight_sheet, KNIGHT_ANIMATION_STEPS)
+brawler_2 = Brawlers(700, 310, samurai_sheet, SAMURAI_ANIMATION_STEPS)
 
 #game loop
 run = True
@@ -34,6 +54,10 @@ while run:
 
     #drawing the background
     draw_bg()
+
+    #Display of Health Bar
+    draw_health_bar(brawler_1.health, 20, 20)
+    draw_health_bar(brawler_2.health, 580, 20)
 
     #calling the movement method from fighters
     brawler_1.move(CANVAS_WIDTH, CANVAS_HEIGHT, screen, brawler_2)
